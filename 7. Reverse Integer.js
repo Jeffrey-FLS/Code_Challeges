4abffa0190e495846b547903cd768bc1eab4966a
Jeff <JP7Kode@gmail.com>
2020-02-16 17:56:03 -0500

    working on 7. Reverse Integer

---
+++ 7. Reverse Integer.js
@@ -0,0 +1,15 @@
+
+/**
+ * @param {number} x
+ * @return {number}
+ */
+const reverse = function(x) {
+    const strNum = x.toString();
+
+
+    console.log(`
+        strNum is ${strNum}
+    `);
+};
+
+reverse(123);
