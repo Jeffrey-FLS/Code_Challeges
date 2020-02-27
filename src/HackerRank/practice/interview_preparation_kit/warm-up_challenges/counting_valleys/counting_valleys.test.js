6b05574bf3c4a4f5365722d473a7764e6a52aeff
Jeff <JP7Kode@gmail.com>
2020-02-27 09:46:24 -0500

    Setting tests on couting_valleys

--- src/HackerRank/practice/interview_preparation_kit/warm-up_challenges/counting_valleys/counting_valleys.test.js
+++ src/HackerRank/practice/interview_preparation_kit/warm-up_challenges/counting_valleys/counting_valleys.test.js
@@ -4,7 +4,7 @@ import {countingValleys} from "./counting_valleys";
 
 describe('countingValleys()', () => {
     it('returns negative numbers in reverse order', () => {
-        const arrStrInput = ["U", "D", "D", "D", "U", "D", "U", "U"];
+        const arrStrInput = ["D", "D", "U", "U", "D", "D", "U", "D", "U", "U", "U", "D"];
         const intInput = arrStrInput.length;
 
         const expected = 1;
@@ -12,9 +12,12 @@ describe('countingValleys()', () => {
         expect(actual).to.deep.equal(expected);
     });
 
-    // it('returns the numbers with ending zeros in reverse order', () => {
-    //     const expected = 21;
-    //     const actual = reverse(120);
-    //     expect(actual).to.deep.equal(expected);
-    // });
+    it('returns negative numbers in reverse order', () => {
+        const arrStrInput = ["U", "D", "D", "D", "U", "D", "U", "U"];
+        const intInput = arrStrInput.length;
+
+        const expected = 1;
+        const actual = countingValleys(intInput, arrStrInput);
+        expect(actual).to.deep.equal(expected);
+    });
 });
--- src/HackerRank/practice/interview_preparation_kit/warm-up_challenges/sock_merchant/sock_merchant.test.js
+++ src/HackerRank/practice/interview_preparation_kit/warm-up_challenges/sock_merchant/sock_merchant.test.js
@@ -1,16 +1,16 @@
-
-import { expect } from "chai";
-import {sockMerchant} from "./sock_merchant";
-
-describe('sockMerchant()', () => {
-   it('returns the numbers in reverse order', () => {
-
-        const coloredSocks = [10, 20, 20, 10, 10, 30, 50, 10, 20];
-        const numOfSocks = coloredSocks.length;
-
-        const expected = 3;
-
-        const actual = sockMerchant(numOfSocks, coloredSocks);
-        expect(actual).to.deep.equal(expected);
-   });
-});
+//
+// import { expect } from "chai";
+// import {sockMerchant} from "./sock_merchant";
+//
+// describe('sockMerchant()', () => {
+//    it('returns the numbers in reverse order', () => {
+//
+//         const coloredSocks = [10, 20, 20, 10, 10, 30, 50, 10, 20];
+//         const numOfSocks = coloredSocks.length;
+//
+//         const expected = 3;
+//
+//         const actual = sockMerchant(numOfSocks, coloredSocks);
+//         expect(actual).to.deep.equal(expected);
+//    });
+// });
