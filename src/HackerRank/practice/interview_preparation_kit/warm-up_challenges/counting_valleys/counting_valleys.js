cdf6e8522711c65da9d06d148329ff53917cf008
Jeff <JP7Kode@gmail.com>
2020-03-29 14:39:21 -0400

    Done with counting_valleys

--- src/HackerRank/practice/interview_preparation_kit/warm-up_challenges/counting_valleys/counting_valleys.js
+++ src/HackerRank/practice/interview_preparation_kit/warm-up_challenges/counting_valleys/counting_valleys.js
@@ -1,51 +1,30 @@
 "use strict";
 
-// for (let i = 0; i < n; i++) {
-//     if (arrStrPath[i] === "D" && prevPath.length === 1) {
-//       valleyCount++;
-//       prevPath = prevPath + "D";
-//     } else if (arrStrPath[i] === "D" && !prevPath) {
-//       prevPath = "D";
-//     } else {
-//       prevPath = "";
-//     }
-//   }
-
-// const valleyCountCalc = (previousPath, newPath) => {
-//     if (newPath === )
-// }
-
 // Complete the countingValleys function below.
 export function countingValleys(n, s) {
-  let declineValleyCount = 0;
-  let inclineValleyCount = 0;
-  let prevPath = "";
   const arrStrPath = s.split("");
+  let intLevelPath = 0;
+  let valleyCount = 0;
+  let isBelowSeaLevel = false;
 
   for (let i = 0; i < n; i++) {
-    if (arrStrPath[i] === "U" && prevPath === "U") {
-      declineValleyCount++;
-      prevPath = prevPath + "U";
-      continue;
+    if (arrStrPath[i] === "U") {
+      ++intLevelPath;
+    } else if (arrStrPath[i] === "D") {
+      --intLevelPath;
     }
 
-    if (arrStrPath[i] === "D" && prevPath === "D") {
-      inclineValleyCount++;
-      prevPath = prevPath + "D";
+    if (intLevelPath < 0 && isBelowSeaLevel) {
       continue;
+    } else if (intLevelPath < 0) {
+      isBelowSeaLevel = true;
+      ++valleyCount;
+    } else if (intLevelPath >= 0) {
+      isBelowSeaLevel = false;
     }
-
-    prevPath = arrStrPath[i];
   }
 
-  // QUOKKA TESTS
-  declineValleyCount;
-  inclineValleyCount;
-  prevPath;
-  arrStrPath;
-  // ----------------------------------------------------------------
-
-  return Math.abs(inclineValleyCount - declineValleyCount);
+  return valleyCount;
 }
 
 console.log(countingValleys(12, "DDUUDDUDUUUD"));
