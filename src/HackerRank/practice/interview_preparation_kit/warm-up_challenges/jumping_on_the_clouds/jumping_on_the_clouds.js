e094ed9426de97b937076db9be492f92cf518c5e
Jeff <JP7Kode@gmail.com>
2020-03-29 15:02:49 -0400

    Pushing to Git

---
+++ src/HackerRank/practice/interview_preparation_kit/warm-up_challenges/jumping_on_the_clouds/jumping_on_the_clouds.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Complete the jumpingOnClouds function below.
+function jumpingOnClouds(c) {
+
+
+}
+
+// Sample Test Cases
+console.log(jumpingOnClouds("0, 0, 1, 0, 0, 1, 0"));
+// console.log(jumpingOnClouds("0, 0, 0, 1, 0, 0"));
