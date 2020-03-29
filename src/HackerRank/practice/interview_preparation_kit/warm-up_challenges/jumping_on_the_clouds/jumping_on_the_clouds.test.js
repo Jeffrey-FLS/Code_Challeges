16090ab026feb4399bbbcb64ae756cf55fd51c6b
Jeff <JP7Kode@gmail.com>
2020-03-29 14:41:06 -0400

    Set jumping_on_the_clouds

---
+++ src/HackerRank/practice/interview_preparation_kit/warm-up_challenges/jumping_on_the_clouds/jumping_on_the_clouds.test.js
@@ -0,0 +1,17 @@
+//
+// import { expect } from "chai";
+// import {sockMerchant} from "./sock_merchant";
+//
+// describe('sockMerchant()', () => {
+//     it('returns negative numbers in reverse order', () => {
+//         const expected = -321;
+//         const actual = reverse(-123);
+//         expect(actual).to.deep.equal(expected);
+//     });
+//
+//     it('returns the numbers with ending zeros in reverse order', () => {
+//         const expected = 21;
+//         const actual = reverse(120);
+//         expect(actual).to.deep.equal(expected);
+//     });
+// });
