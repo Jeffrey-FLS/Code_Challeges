913948d7214421cd9fb415cccc9d5562fdcbdaf8
Jeff <JP7Kode@gmail.com>
2020-03-31 12:54:25 -0400

    Working on jumping_on_the_clouds

--- src/HackerRank/practice/interview_preparation_kit/warm-up_challenges/jumping_on_the_clouds/jumping_on_the_clouds.js
+++ src/HackerRank/practice/interview_preparation_kit/warm-up_challenges/jumping_on_the_clouds/jumping_on_the_clouds.js
@@ -2,10 +2,24 @@
 
 // Complete the jumpingOnClouds function below.
 function jumpingOnClouds(c) {
+    const arrIntCloudPath = [...c+''].map(n => +n);
+    let arrIntPath = [];
+    // let pathCount = 0;
 
+    arrIntCloudPath.filter((val, index) => (!val && index % 2 === 0) && arrIntPath.push(index));
+    // arrIntCloudPath.filter((val, index) => (!val) && arrIntPath.push(index));
 
+    // pathCount.filter
+    
+
+    // QUOKKA TESTS
+    arrIntCloudPath;
+    arrIntPath;
+
+    return arrIntPath.length;
 }
 
 // Sample Test Cases
-console.log(jumpingOnClouds("0, 0, 1, 0, 0, 1, 0"));
-// console.log(jumpingOnClouds("0, 0, 0, 1, 0, 0"));
+console.log(jumpingOnClouds("000010")); // Expected 3
+console.log(jumpingOnClouds("0010010")); // Expected 4
+console.log(jumpingOnClouds("000100")); // Expected 3
