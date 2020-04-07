233aa11781860fbcd38d1c1e842e581d072d1c89
Jeff <JP7Kode@gmail.com>
2020-04-07 14:13:27 -0400

    I have multiple solutions to repeat_string

---
+++ src/HackerRank/practice/interview_preparation_kit/warm-up_challenges/repeated_string/repeated_string (alternative).js
@@ -0,0 +1,49 @@
+"use strict";
+
+// Complete the repeatedString function below.
+function repeatedString(str, num) {
+  const dividend = parseInt(num / str.length);
+  const difference = num - (dividend * str.length);
+
+  let newStr = [];
+  let charACount = 0;
+
+
+  // QUOKKA --------------------------------------------------------------------
+  console.log(str.length);
+  dividend;
+  difference;
+  // ---------------------------------------------------------------------------
+
+
+
+  // CORRECT ANSWER ------------------------------------------------------------
+  // newStr = str.repeat(dividend) + str.slice(0, difference);
+  // newStr.split("").forEach(ele => (ele === "a") && charACount++);
+  // ---------------------------------------------------------------------------
+
+
+
+  // ALTERNATIVE ---------------------------------------------------------------
+  for (let i = 0; i < dividend; i++) {
+    newStr.push(str)
+  }
+
+  (difference) && newStr.push(str.slice(0, difference));
+  newStr.join("").split("").forEach(ele => (ele === "a") && charACount++);
+  // ---------------------------------------------------------------------------
+
+  
+
+  // QUOKKA --------------------------------------------------------------------
+  newStr;
+  charACount;
+  // ---------------------------------------------------------------------------
+
+  return charACount;
+}
+
+console.log(repeatedString("abcac", 10)); // Expected 4
+console.log(repeatedString("aba", 10)); // Expected 7
+console.log(repeatedString("a", 1000)) // Expected 1000000000000
+// console.log(repeatedString("a", 1000000000000)) // Expected 1000000000000
--- src/HackerRank/practice/interview_preparation_kit/warm-up_challenges/repeated_string/repeated_string.js
+++ src/HackerRank/practice/interview_preparation_kit/warm-up_challenges/repeated_string/repeated_string.js
@@ -11,19 +11,22 @@ function repeatedString(str, num) {
 
   // Populate new string arrNewStr
   //
+  let i = 0;
 
-  for (let i = 0; i <= num; 0) {
+  console.log("HEELLOO");
+
+  while (i <= num) {
     // let k = 0;
 
     // while (k < str.length || i < num) {
 
     // }
 
-    i;
+    // i;
 
     for (let k = 0; k < str.length; k++) {
       // QUOKKA TESTS ----------------------------------------------------------
-      // k;
+      console.log(k);
 
       // str;
 
@@ -46,25 +49,24 @@ function repeatedString(str, num) {
 
       i++;
     } // end for loop
-
-    i;
-  }
+  } //end while loop
 
   // for (let i = 0; i < dividend; i++) {
   //     arrNewStr.push(str);
   // }
 
   // QUOKKA TEST
-  dividend;
-  stringLength;
+  // dividend;
+  // stringLength;
+  // charACount;
 
   //   arrNewStr;
-  console.log(arrNewStr.length);
+  // console.log(arrNewStr.length);
 
   //   return arrNewStr.join("").length;
   return charACount;
 }
 
 // console.log(repeatedString("abcac", 10)); // Expected 4
-console.log(repeatedString("aba", 10)); // Expected 7
+repeatedString("aba", 10); // Expected 7
 // repeatedString("a", 1000000000000) // Expected 1000000000000
