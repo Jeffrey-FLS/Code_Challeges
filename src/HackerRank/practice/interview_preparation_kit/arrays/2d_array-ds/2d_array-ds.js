cbf80d804c4d97193e47a142f902d28b25931476
Jeff <JP7Kode@gmail.com>
2020-04-13 11:44:44 -0400

    Working on 2d_array-ds

--- src/HackerRank/practice/interview_preparation_kit/arrays/2d_array-ds/2d_array-ds.js
+++ src/HackerRank/practice/interview_preparation_kit/arrays/2d_array-ds/2d_array-ds.js
@@ -1,11 +1,14 @@
-
 "use strict";
 
 // Pushing to commit
 
 // Complete the hourglassSum function below.
 function hourglassSum(arr) {
+    
 
 
 }
 
+console.log(hourglassSum([1,1,1,0,0,0,0,1,0,0,0,0,1,1,1,0,0,0,0,0,2,4,4,0,0,0,0,2,0,0,0,0,1,2,4,0])); // Expect 19
+// console.log(hourglassSum([1,1,1,0,0,0,0,1,0,0,0,0,1,1,1,0,0,0,0,9,2,-4,-4,0,0,0,0,-2,0,0,0,0,-1,-2,-4,0])); // Expect 13
+// console.log(hourglassSum([-9,-9,-9,1,1,1,0,-9,0,4,3,2,-9,-9,-9,1,2,3,0,0,8,6,6,0,0,0,0,-2,0,0,0,0,1,2,4,0])); // Expect 28
